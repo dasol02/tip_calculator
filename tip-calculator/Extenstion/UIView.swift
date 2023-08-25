1bd83334638d7d825ec4617bd2804b58280eb5f6
dasol02 <leedasol2169@gmail.com>
2023-08-25 10:54:51 +0900

    # Update BillnputView

--- tip-calculator/Extenstion/UIView.swift
+++ tip-calculator/Extenstion/UIView.swift
@@ -19,6 +19,11 @@ extension UIView {
         backgroundColor = nil
         layer.backgroundColor = backgroundCGColor
     }
+    
+    func addCornerRadius(radius: CGFloat) {
+        layer.masksToBounds = false
+        layer.cornerRadius = radius
+    }
 }
 
 
---
+++ tip-calculator/View/HeaderView.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderView.swift
+//  tip-calculator
+//
+//  Created by dasol lee on 2023/08/25.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+    
+    }
+}
+
