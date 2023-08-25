785d7f52c0267387788a2a7fb217a23b7077f8dc
dasol02 <leedasol2169@gmail.com>
2023-08-25 11:40:32 +0900

    # Update Common HeaderView

--- tip-calculator/View/BillInputView.swift
+++ tip-calculator/View/BillInputView.swift
@@ -10,7 +10,11 @@ import UIKit
 class BillInputView: UIView {
     
     private let headerView: HeaderView = {
-        return HeaderView()
+        let view = HeaderView()
+        view.configure(
+            topText: "Enter",
+            bottomText: "your bill")
+        return view
     }()
     
     private let textFieldContainerView: UIView = {
