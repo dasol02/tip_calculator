84db5bef8716a61cc6d6eb0ca2cc8b449ca00c39
dasol02 <leedasol2169@gmail.com>
2023-08-30 10:58:35 +0900

    # Update Display Result View

---
+++ tip-calculator/Extenstion/Double.swift
@@ -0,0 +1,20 @@
+//
+//  Double.swift
+//  tip-calculator
+//
+//  Created by dasol lee on 2023/08/30.
+//
+
+import Foundation
+
+extension Double {
+    var currencyFormatted: String {
+        var isWholeNumber: Bool {
+            isZero ? true : !isNormal ? false: self == rounded()
+        }
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = isWholeNumber ? 0 : 2
+        return formatter.string(for: self) ?? ""
+    }
+}
--- tip-calculator/View/ResultView.swift
+++ tip-calculator/View/ResultView.swift
@@ -49,15 +49,25 @@ class ResultView: UIView {
         return stackView
     }()
     
+    private let totalTipView: AmountView = {
+        let view = AmountView(
+            title: "Total tip",
+            textAlignment: .right)
+        return view
+    }()
+    
+    private let totalBillView: AmountView = {
+        let view = AmountView(
+            title: "Total bill",
+            textAlignment: .left)
+        return view
+    }()
+    
     private lazy var hStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [
-            AmountView(
-                title: "Total bill",
-                textAlignment: .left),
+            totalBillView,
             UIView(),
-            AmountView(
-                title: "Total tip",
-                textAlignment: .right),
+            totalTipView,
         ])
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
@@ -72,6 +82,18 @@ class ResultView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(result: Result) {
+        let text = NSMutableAttributedString(
+            string: result.amointPerPerson.currencyFormatted,
+            attributes: [.font: ThemeFont.bold(ofSize: 48)])
+        text.addAttributes([
+            .font: ThemeFont.bold(ofSize: 24)
+            ], range: NSMakeRange(0, 1))
+        amoutPerPersonLabel.attributedText = text
+        totalBillView.configure(amount: result.totalBill)
+        totalTipView.configure(amount: result.totalTip)
     }
     
     private func layout() {
